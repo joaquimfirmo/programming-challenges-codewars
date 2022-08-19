6166ed1713cbeeb7e5ce5972b1077d0faae90413
Joaquim Firmo <joaquimnt18@gmail.com>
2022-08-19 14:16:06 -0300

    booleanToString

---
+++ src/arquivos/codewars/booleanToString.js
@@ -0,0 +1,5 @@
+// Implemente uma função que converta o valor booleano fornecido em sua representação de string.
+
+// Nota: Somente entradas válidas serão fornecidas.
+
+module.exports = (b) => b ? 'true' : 'false';
