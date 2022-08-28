1d67359e6338eaa2027ed3665d87e2076671669b
Joaquim Firmo <joaquimnt18@gmail.com>
2022-08-27 21:59:06 -0300

    chore

--- src/__tests__/codewars/mergeObj.spec.js
+++ src/__tests__/codewars/mergeObj.spec.js
@@ -2,12 +2,12 @@ const mergeObj = require('../../files/mergeObj');
 
 
 describe("Merge Objetos", () => {
-    test("Deve fazer o merge dos dois objetos passados como paramentros, caso a o 3 paramentro seja true.", () => {
+    test("Deve fazer o merge dos dois objetos passados como paramentros, caso o 3 paramentro seja true.", () => {
         expect(mergeObj({nome:'Joaquim', sobrenome: "Firmo"}, {sexo: 'M', idade: 25}, true)).toEqual(
             {nome:'Joaquim', sobrenome: "Firmo", sexo: 'M', idade: 25});         
     });
 
-    test("Não deve fazer o merge dos dois objetos passados como paramentros, caso a o 3 paramentro seja true.", () => {
+    test("Não deve fazer o merge dos dois objetos passados como paramentros, caso  o 3 paramentro seja true.", () => {
         expect(mergeObj({nome:'Joaquim', sobrenome: "Firmo"}, {sexo: 'M', idade: 25, altura: 158}, false)).toEqual(
             {nome:'Joaquim', sobrenome: "Firmo"});  
        
